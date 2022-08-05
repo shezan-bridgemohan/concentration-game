2c54fc49462e20299a139d6db350142edc69324e
Shezan Bridgemohan <shezanb@discovery.co.za>
2022-08-05 10:22:55 +0200

    updated image paths in ts file

--- docs/main.js
+++ docs/main.js
@@ -230,378 +230,378 @@ class GameComponent {
                 value: "A",
                 color: "black",
                 id: "Clubs Ace",
-                img: "../../assets/img/cards/clubs-A.png"
+                img: "./assets/img/cards/clubs-A.png"
             },
             {
                 suite: "clubs",
                 value: "2",
                 color: "black",
                 id: "Clubs 2",
-                img: "../../assets/img/cards/clubs-2.png"
+                img: "./assets/img/cards/clubs-2.png"
             },
             {
                 suite: "clubs",
                 value: "3",
                 color: "black",
                 id: "Clubs 3",
-                img: "../../assets/img/cards/clubs-3.png"
+                img: "./assets/img/cards/clubs-3.png"
             },
             {
                 suite: "clubs",
                 value: "4",
                 color: "black",
                 id: "Clubs 4",
-                img: "../../assets/img/cards/clubs-4.png"
+                img: "./assets/img/cards/clubs-4.png"
             },
             {
                 suite: "clubs",
                 value: "5",
                 color: "black",
                 id: "Clubs 5",
-                img: "../../assets/img/cards/clubs-5.png"
+                img: "./assets/img/cards/clubs-5.png"
             },
             {
                 suite: "clubs",
                 value: "6",
                 color: "black",
                 id: "Clubs 6",
-                img: "../../assets/img/cards/clubs-6.png"
+                img: "./assets/img/cards/clubs-6.png"
             },
             {
                 suite: "clubs",
                 value: "7",
                 color: "black",
                 id: "Clubs 7",
-                img: "../../assets/img/cards/clubs-7.png"
+                img: "./assets/img/cards/clubs-7.png"
             },
             {
                 suite: "clubs",
                 value: "8",
                 color: "black",
                 id: "Clubs 8",
-                img: "../../assets/img/cards/clubs-8.png"
+                img: "./assets/img/cards/clubs-8.png"
             },
             {
                 suite: "clubs",
                 value: "9",
                 color: "black",
                 id: "Clubs 9",
-                img: "../../assets/img/cards/clubs-9.png"
+                img: "./assets/img/cards/clubs-9.png"
             },
             {
                 suite: "clubs",
                 value: "10",
                 color: "black",
                 id: "Clubs 10",
-                img: "../../assets/img/cards/clubs-10.png"
+                img: "./assets/img/cards/clubs-10.png"
             },
             {
                 suite: "clubs",
                 value: "J",
                 color: "black",
                 id: "Clubs Jack",
-                img: "../../assets/img/cards/clubs-J.png"
+                img: "./assets/img/cards/clubs-J.png"
             },
             {
                 suite: "clubs",
                 value: "Q",
                 color: "black",
                 id: "Clubs Queen",
-                img: "../../assets/img/cards/clubs-Q.png"
+                img: "./assets/img/cards/clubs-Q.png"
             },
             {
                 suite: "clubs",
                 value: "K",
                 color: "black",
                 id: "Clubs King",
-                img: "../../assets/img/cards/clubs-K.png"
+                img: "./assets/img/cards/clubs-K.png"
             },
             {
                 suite: "diamonds",
                 value: "A",
                 color: "red",
                 id: "Diamonds Ace",
-                img: "../../assets/img/cards/diamonds-A.png"
+                img: "./assets/img/cards/diamonds-A.png"
             },
             {
                 suite: "diamonds",
                 value: "2",
                 color: "red",
                 id: "Diamonds 2",
-                img: "../../assets/img/cards/diamonds-2.png"
+                img: "./assets/img/cards/diamonds-2.png"
             },
             {
                 suite: "diamonds",
                 value: "3",
                 color: "red",
                 id: "Diamonds 3",
-                img: "../../assets/img/cards/diamonds-3.png"
+                img: "./assets/img/cards/diamonds-3.png"
             },
             {
                 suite: "diamonds",
                 value: "4",
                 color: "red",
                 id: "Diamonds 4",
-                img: "../../assets/img/cards/diamonds-4.png"
+                img: "./assets/img/cards/diamonds-4.png"
             },
             {
                 suite: "diamonds",
                 value: "5",
                 color: "red",
                 id: "Diamonds 5",
-                img: "../../assets/img/cards/diamonds-5.png"
+                img: "./assets/img/cards/diamonds-5.png"
             },
             {
                 suite: "diamonds",
                 value: "6",
                 color: "red",
                 id: "Diamonds 6",
-                img: "../../assets/img/cards/diamonds-6.png"
+                img: "./assets/img/cards/diamonds-6.png"
             },
             {
                 suite: "diamonds",
                 value: "7",
                 color: "red",
                 id: "Diamonds 7",
-                img: "../../assets/img/cards/diamonds-7.png"
+                img: "./assets/img/cards/diamonds-7.png"
             },
             {
                 suite: "diamonds",
                 value: "8",
                 color: "red",
                 id: "Diamonds 8",
-                img: "../../assets/img/cards/diamonds-8.png"
+                img: "./assets/img/cards/diamonds-8.png"
             },
             {
                 suite: "diamonds",
                 value: "9",
                 color: "red",
                 id: "Diamonds 9",
-                img: "../../assets/img/cards/diamonds-9.png"
+                img: "./assets/img/cards/diamonds-9.png"
             },
             {
                 suite: "diamonds",
                 value: "10",
                 color: "red",
                 id: "Diamonds 10",
-                img: "../../assets/img/cards/diamonds-10.png"
+                img: "./assets/img/cards/diamonds-10.png"
             },
             {
                 suite: "diamonds",
                 value: "J",
                 color: "red",
                 id: "Diamonds Jack",
-                img: "../../assets/img/cards/diamonds-J.png"
+                img: "./assets/img/cards/diamonds-J.png"
             },
             {
                 suite: "diamonds",
                 value: "Q",
                 color: "red",
                 id: "Diamonds Queen",
-                img: "../../assets/img/cards/diamonds-Q.png"
+                img: "./assets/img/cards/diamonds-Q.png"
             },
             {
                 suite: "diamonds",
                 value: "K",
                 color: "red",
                 id: "Diamonds King",
-                img: "../../assets/img/cards/diamonds-K.png"
+                img: "./assets/img/cards/diamonds-K.png"
             },
             {
                 suite: "hearts",
                 value: "A",
                 color: "red",
                 id: "Hearts Ace",
-                img: "../../assets/img/cards/hearts-A.png"
+                img: "./assets/img/cards/hearts-A.png"
             },
             {
                 suite: "hearts",
                 value: "2",
                 color: "red",
                 id: "Hearts 2",
-                img: "../../assets/img/cards/hearts-2.png"
+                img: "./assets/img/cards/hearts-2.png"
             },
             {
                 suite: "hearts",
                 value: "3",
                 color: "red",
                 id: "Hearts 3",
-                img: "../../assets/img/cards/hearts-3.png"
+                img: "./assets/img/cards/hearts-3.png"
             },
             {
                 suite: "hearts",
                 value: "4",
                 color: "red",
                 id: "Hearts 4",
-                img: "../../assets/img/cards/hearts-4.png"
+                img: "./assets/img/cards/hearts-4.png"
             },
             {
                 suite: "hearts",
                 value: "5",
                 color: "red",
                 id: "Hearts 5",
-                img: "../../assets/img/cards/hearts-5.png"
+                img: "./assets/img/cards/hearts-5.png"
             },
             {
                 suite: "hearts",
                 value: "6",
                 color: "red",
                 id: "Hearts 6",
-                img: "../../assets/img/cards/hearts-6.png"
+                img: "./assets/img/cards/hearts-6.png"
             },
             {
                 suite: "hearts",
                 value: "7",
                 color: "red",
                 id: "Hearts 7",
-                img: "../../assets/img/cards/hearts-7.png"
+                img: "./assets/img/cards/hearts-7.png"
             },
             {
                 suite: "hearts",
                 value: "8",
                 color: "red",
                 id: "Hearts 8",
-                img: "../../assets/img/cards/hearts-8.png"
+                img: "./assets/img/cards/hearts-8.png"
             },
             {
                 suite: "hearts",
                 value: "9",
                 color: "red",
                 id: "Hearts 9",
-                img: "../../assets/img/cards/hearts-9.png"
+                img: "./assets/img/cards/hearts-9.png"
             },
             {
                 suite: "hearts",
                 value: "10",
                 color: "red",
                 id: "Hearts 10",
-                img: "../../assets/img/cards/hearts-10.png"
+                img: "./assets/img/cards/hearts-10.png"
             },
             {
                 suite: "hearts",
                 value: "J",
                 color: "red",
                 id: "Hearts Jack",
-                img: "../../assets/img/cards/hearts-J.png"
+                img: "./assets/img/cards/hearts-J.png"
             },
             {
                 suite: "hearts",
                 value: "Q",
                 color: "red",
                 id: "Hearts Queen",
-                img: "../../assets/img/cards/hearts-Q.png"
+                img: "./assets/img/cards/hearts-Q.png"
             },
             {
                 suite: "hearts",
                 value: "K",
                 color: "red",
                 id: "Hearts King",
-                img: "../../assets/img/cards/hearts-K.png"
+                img: "./assets/img/cards/hearts-K.png"
             },
             {
                 suite: "spades",
                 value: "A",
                 color: "black",
                 id: "Spades Ace",
-                img: "../../assets/img/cards/spades-A.png"
+                img: "./assets/img/cards/spades-A.png"
             },
             {
                 suite: "spades",
                 value: "2",
                 color: "black",
                 id: "Spades 2",
-                img: "../../assets/img/cards/spades-2.png"
+                img: "./assets/img/cards/spades-2.png"
             },
             {
                 suite: "spades",
                 value: "3",
                 color: "black",
                 id: "Spades 3",
-                img: "../../assets/img/cards/spades-3.png"
+                img: "./assets/img/cards/spades-3.png"
             },
             {
                 suite: "spades",
                 value: "4",
                 color: "black",
                 id: "Spades 4",
-                img: "../../assets/img/cards/spades-4.png"
+                img: "./assets/img/cards/spades-4.png"
             },
             {
                 suite: "spades",
                 value: "5",
                 color: "black",
                 id: "Spades 5",
-                img: "../../assets/img/cards/spades-5.png"
+                img: "./assets/img/cards/spades-5.png"
             },
             {
                 suite: "spades",
                 value: "6",
                 color: "black",
                 id: "Spades 6",
-                img: "../../assets/img/cards/spades-6.png"
+                img: "./assets/img/cards/spades-6.png"
             },
             {
                 suite: "spades",
                 value: "7",
                 color: "black",
                 id: "Spades 7",
-                img: "../../assets/img/cards/spades-7.png"
+                img: "./assets/img/cards/spades-7.png"
             },
             {
                 suite: "spades",
                 value: "8",
                 color: "black",
                 id: "Spades 8",
-                img: "../../assets/img/cards/spades-8.png"
+                img: "./assets/img/cards/spades-8.png"
             },
             {
                 suite: "spades",
                 value: "9",
                 color: "black",
                 id: "Spades 9",
-                img: "../../assets/img/cards/spades-9.png"
+                img: "./assets/img/cards/spades-9.png"
             },
             {
                 suite: "spades",
                 value: "10",
                 color: "black",
                 id: "Spades 10",
-                img: "../../assets/img/cards/spades-10.png"
+                img: "./assets/img/cards/spades-10.png"
             },
             {
                 suite: "spades",
                 value: "J",
                 color: "black",
                 id: "Spades Jack",
-                img: "../../assets/img/cards/spades-J.png"
+                img: "./assets/img/cards/spades-J.png"
             },
             {
                 suite: "spades",
                 value: "Q",
                 color: "black",
                 id: "Spades Queen",
-                img: "../../assets/img/cards/spades-Q.png"
+                img: "./assets/img/cards/spades-Q.png"
             },
             {
                 suite: "spades",
                 value: "K",
                 color: "black",
                 id: "Spades King",
-                img: "../../assets/img/cards/spades-K.png"
+                img: "./assets/img/cards/spades-K.png"
             },
             {
                 suite: "red",
                 value: "joker",
                 color: "joker",
                 id: "red-joker",
-                img: "../../assets/img/cards/joker-red.png"
+                img: "./assets/img/cards/joker-red.png"
             },
             {
                 suite: "black",
                 value: "joker",
                 color: "joker",
                 id: "black-joker",
-                img: "../../assets/img/cards/joker-black.png"
+                img: "./assets/img/cards/joker-black.png"
             }
         ];
         this.shuffledCards = this.shuffle(this.playingCards);
